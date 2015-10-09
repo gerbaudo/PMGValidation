gerbaudo/PMGValidation

commit d1a7aed830e6bdeb99beb2ff5094b13781520f48

cleanup warnings

Root/TruthReader.cxx
@@ -387,6 +387,7 @@ EL::StatusCode TruthReader :: finalize ()
   // gets called on worker nodes that processed input events.
 
   xAOD::TEvent* event = wk()->xaodEvent();
+  Info("finalize()", "Number of events = %lli", event->getEntries() );
 
   return EL::StatusCode::SUCCESS;
 }

Root/AtlasUtils.cxx
@@ -100,19 +100,12 @@ TGraphAsymmErrors* myTGraphErrorsDivide(TGraphAsymmErrors* g1,TGraphAsymmErrors*
   double dy1h=0., dy1l=0.;
   double dy2h=0., dy2l=0.;
 
-  double* X1 = g1->GetX();
-  double* Y1 = g1->GetY();
   double* EXhigh1 = g1->GetEXhigh();
-  double* EXlow1 =  g1->GetEXlow();
+  double* EXlow1  = g1->GetEXlow();
   double* EYhigh1 = g1->GetEYhigh();
-  double* EYlow1 =  g1->GetEYlow();
-
-  double* X2 = g2->GetX();
-  double* Y2 = g2->GetY();
-  double* EXhigh2 = g2->GetEXhigh();
-  double* EXlow2 =  g2->GetEXlow();
+  double* EYlow1  = g1->GetEYlow();
   double* EYhigh2 = g2->GetEYhigh();
-  double* EYlow2 =  g2->GetEYlow();
+  double* EYlow2  = g2->GetEYlow();
 
   for (int i=0; i<g1->GetN(); i++) {
     g1->GetPoint(i,x1,y1);

util/MergeHisto.cxx
@@ -30,7 +30,6 @@ int main( int argc, char* argv[] ) {
 
   TFile *f1 = new TFile( file1 );
   TFile *f2 = new TFile( file2 );
-  TFile *fFinal = new TFile( "test.root" ,"recreate");
 
   TString h_name[25] = {"h_jetN","h_jetPt","h_jetE","h_jetEta","h_jetPhi",
 			"h_bjetN","h_bjetPt","h_bjetE","h_bjetEta","h_bjetPhi",

util/TruthPlot.cxx
@@ -1,3 +1,5 @@
+#include "PMGValidation/TruthReader.h"
+
 #include "xAODRootAccess/Init.h"
 #include "SampleHandler/SampleHandler.h"
 #include "SampleHandler/ToolsDiscovery.h"
@@ -5,16 +7,18 @@
 #include "EventLoop/Job.h"
 #include "EventLoop/DirectDriver.h"
 #include "SampleHandler/DiskListLocal.h"
+
 #include <TSystem.h>
 
-#include "PMGValidation/TruthReader.h"
+#include <string>
+using namespace std;
 
 int main( int argc, char* argv[] ) {
 
   // Take the submit directory from the input if provided:
-  std::string submitDir = "submitDir";
-  char* inputFilePath = gSystem->ExpandPathName ("/data1/atlas/berlendis/Data/4top/");
-  char* filename = "DAOD_TRUTH1.mc15_4top.pool.root";
+  string submitDir = "submitDir";
+  string inputFilePath = gSystem->ExpandPathName ("/data1/atlas/berlendis/Data/4top/");
+  string filename = "DAOD_TRUTH1.mc15_4top.pool.root";
 
   if( argc > 1 ) inputFilePath = argv[ 1 ];
   if( argc > 2 ) filename = argv[ 2 ];

commit 491630ad4b6df938553d939f7abd7222a5e88e8e

avoid crash (introduced in d1a7aed)

Root/TruthReader.cxx
@@ -386,9 +386,6 @@ EL::StatusCode TruthReader :: finalize ()
   // merged.  This is different from histFinalize() in that it only
   // gets called on worker nodes that processed input events.
 
-  xAOD::TEvent* event = wk()->xaodEvent();
-  Info("finalize()", "Number of events = %lli", event->getEntries() );
-
   return EL::StatusCode::SUCCESS;
 }
 